da69cb0d67840c1dcf44080db86b15161a066b9d
aeno-ai <ronrabaya6@gmail.com>
2025-04-08 09:05:27 +0800

    newest

--- script.js
+++ script.js
@@ -2,44 +2,6 @@ document.querySelector(".menu-toggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("toggle");
 });
 
-document.addEventListener("DOMContentLoaded", function () { //DOMContent = hinihintay muna mag load
-    let darkModeButton = document.querySelector(".dark-mode button");
-    let darkModeButtonMobile = document.querySelector(".dark-mode-mobile button");
-    let body = document.body;
-    let themeIcon = document.querySelector(".dark-mode button img");
-    let themeMenu = document.querySelector(".menu-toggle img");
-
-    function update() {
-        if (body.classList.contains("dark-theme")) {
-            themeIcon.src = themeIcon.getAttribute("theme-sun");
-            themeMenu.src = themeMenu.getAttribute("menu-line-dark");
-        } else {
-            themeIcon.src = themeIcon.getAttribute("theme-moon");
-            themeMenu.src = themeMenu.getAttribute("menu-line");
-        }
-    }
-
-    if (localStorage.getItem("theme") === "dark") {
-        body.classList.add("dark-theme");
-        update();
-    }
-
-    function toggleDarkMode() {
-        body.classList.toggle("dark-theme");
-        update();
-        if (body.classList.contains("dark-theme")) {
-            localStorage.setItem("theme", "dark");
-        } else {
-            localStorage.setItem("theme", "light");
-        }
-    }
-
-    darkModeButton.addEventListener("click", toggleDarkMode);
-    if (darkModeButtonMobile) {
-        darkModeButtonMobile.addEventListener("click", toggleDarkMode);
-    }
-});
-
 // BMI PAGE
 function calculateBMI() {
     let weight = document.getElementById("weight").value;
